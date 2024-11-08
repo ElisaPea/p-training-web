f3f3929065d1cee05d796645e2adb68572e4cf77
ElisaPea <eelisapea@gmail.com>
2024-11-08 17:23:30 +0100

    Bacheca

--- src/app/components/landind-page.tsx
+++ src/app/components/landind-page.tsx
@@ -1,5 +1,16 @@
 import { Card, CardBody, Spinner, Skeleton } from "@nextui-org/react";
 import Image from "next/image";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  RadioGroup,
+  Radio,
+} from "@nextui-org/react";
 import { useState } from "react";
 import { secretSantaList } from "./config";
 import { SecretSantaCard } from "./secret-santa-card";
@@ -7,6 +18,7 @@ import { LuSparkles } from "react-icons/lu";
 import { SlPresent } from "react-icons/sl";
 
 export default function LandingPage() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   // <div className="bg-clip-text text-transparent bg-gradient-to-r from-[#D32F2F] to-[#FF5722]">
 
   return (
@@ -30,49 +42,63 @@ export default function LandingPage() {
             🎅🎅🎅🎅🎅
           </CardBody>
         </Card>
-        <Card className="relative overflow-visible border-2 border-red-200 my-8  max-w-[320px]">
-          <div className="absolute top-[-10px] left-[-10px] text-red-600 text-2xl z-10">
-            <SlPresent />
-            {/* <LuSparkles /> */}
-          </div>
-          <div className="absolute top-[-10px] right-[-10px] text-red-600 text-2xl">
-            {/* <LuSparkles /> */}
-            <SlPresent />
-          </div>
-          <CardBody className="flex flex-col gap-2 text-xl font-semibold text-center text-green-700">
-            ISTRUZIONI:
-            <div className="flex flex-col bg-clip-text text-transparent bg-gradient-to-r from-[#D32F2F] to-[#FF5722]">
-              Clicca sul tuo nome per scoprire a chi ti tocca fare il regalo
-              quest'anno! Non barare e non guardare la scheda che non ti
-              appartiene, o subirai le pene dell'inferno......
-              <div>Anzi, provaci. Ti sfido....</div>
-            </div>
-          </CardBody>
-        </Card>
-
-        {/* <div className="text-center text-md my-16 max-w-[820px] border-2 rounded-2xl border-red-500 p-2 bg-gradient-to-r from-[#D32F2F] to-[#FF5722] text-white">
-          Clicca sul tuo nome per scoprire a chi ti tocca fare il regalo
-          quest'anno! Non barare e non guardare la scheda che non ti appartiene,
-          o subirai le pene dell'inferno...... Anzi, provaci. Ti sfido....
-        </div> */}
-
+        {/* <Card className="my-8 shake-animation"> */}
+        <Button
+          onPress={onOpen}
+          className="border-2 border-black my-8 shake-animation text-xl"
+          color="success"
+          endContent={<SlPresent />}
+        >
+          ISTRUZIONI
+        </Button>
+        <Button
+          onPress={() =>
+            window.open(
+              "https://padlet.com/eelisapea/bacheca-natale-739xsuqzvl9mfre1"
+            )
+          }
+          className="border-2 border-black mb-8 text-xl"
+          color="danger"
+          // endContent={<SlPresent />}
+        >
+          Bachecha
+        </Button>
+        {/* </Card> */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 w-full max-w-[1200px]">
           {secretSantaList.map((member, index) => (
             <SecretSantaCard key={index} member={member} />
           ))}
         </div>
       </div>
+      <Modal isOpen={isOpen} placement={"center"} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-2xl">
+                Chefffare:
+              </ModalHeader>
+              <ModalBody>
+                <p className="flex flex-col gap-2 text-xl">
+                  Clicca sul tuo nome per scoprire a chi ti tocca fare il regalo
+                  quest'anno! Non barare e non guardare la scheda che non ti
+                  appartiene, o subirai le pene dell'inferno......
+                  <p>Anzi, provaci. Ti sfido....</p>
+                  <p>
+                    Se schiacci su Bacheca potrai scrivere le tue preferenze di
+                    regalo in uno spazio condiviso da tutti e il tuo Babbo
+                    Natale segreto potrá prendere spunti!
+                  </p>
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={onClose}>
+                  Capito!!
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
-}
-
-{
-  /* <div className="flex gap-4">
-  <Spinner color="default" />
-  <Spinner color="primary" />
-  <Spinner color="secondary" />
-  <Spinner color="success" />
-  <Spinner color="warning" />
-  <Spinner color="danger" />
-</div> */
 }
